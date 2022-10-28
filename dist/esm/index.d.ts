e01f8e67cd944a90a6859fa71fb22106a1acd15d
Alex Ryltsov <ryltsov.alex@gmail.com>
2022-10-28 18:55:15 +0200

    chore: added dist directory to be able to install from the github

---
+++ dist/esm/index.d.ts
@@ -0,0 +1,4 @@
+import type { FileOpenerPlugin } from './definitions.js';
+declare const FileOpener: FileOpenerPlugin;
+export * from './definitions.js';
+export { FileOpener };
---
+++ dist/plugin.js
@@ -0,0 +1,11 @@
+var capacitorFileOpener = (function (exports, core) {
+	'use strict';
+
+	const FileOpener = core.registerPlugin('FileOpener');
+
+	exports.FileOpener = FileOpener;
+
+	return exports;
+
+})({}, capacitorExports);
+//# sourceMappingURL=plugin.js.map
