2cdc8e3ec3032db63d3ea55961190d25025976d7
Alex Ryltsov <ryltsov.alex@gmail.com>
2022-11-01 09:58:10 +0200

    chore: added dist directory to be able to install from the github

---
+++ dist/esm/definitions.d.ts
@@ -0,0 +1,42 @@
+/**
+ * file open method options
+ *
+ * @since 1.0.0
+ */
+export interface FileOpenerOptions {
+    /**
+     * file path
+     *
+     * @since 1.0.0
+     */
+    filePath: string;
+    /**
+     * MIME type (optional)
+     *
+     * @since 1.0.0
+     */
+    contentType?: string;
+    /**
+     * Use the default platform chooser, if true, otherwise:
+     * On Android: it will show "Open File in.." title of the chooser dialog, the system will always present the chooser dialog
+     * even if the user has chosen a default one and if no activity is found to handle the file, the system will still
+     * present a dialog with the specified title and an error message No application can perform this action
+     * On iOS: it will presents a menu restricted to a list of apps capable of opening the current document.
+     * This determination is made based on the document type and on the document types supported by the installed apps.
+     * To support one or more document types, an app must register those types in its Info.plist file
+     * using the CFBundleDocumentTypes key.
+     *
+     * (optional) default value is true
+     *
+     * @since 1.0.0
+     */
+    openWithDefault?: boolean;
+}
+export interface FileOpenerPlugin {
+    /**
+     * Method to open a file.
+     *
+     * @since 1.0.0
+     */
+    open(options: FileOpenerOptions): Promise<void>;
+}
---
+++ dist/plugin.cjs.js
@@ -0,0 +1,8 @@
+'use strict';
+
+var core = require('@capacitor/core');
+
+const FileOpener = core.registerPlugin('FileOpener');
+
+exports.FileOpener = FileOpener;
+//# sourceMappingURL=plugin.cjs.js.map
