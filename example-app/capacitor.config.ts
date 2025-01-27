a5506e4df85917b759a97d0cc648bf3363ec91bd
Alex Ryltsov <ryltsov.alex@gmail.com>
2025-01-27 08:19:13 +0200

    feature: update to capacitor 7
    Changes:
    - migrated to support Capacitor 7
    - added example-app to demo the file opener plugin usage

---
+++ example-app/capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'ryltsov.alex.file.opener.demo',
+  appName: 'example-app',
+  webDir: 'www/browser'
+};
+
+export default config;
